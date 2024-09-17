0bae867e3bb8872f78f0cd58e07737f848a08659
hassan ali <hassanali0111272@gmail.com>
2024-09-17 21:06:39 +0500

    array file added

---
+++ arrays/script.js
@@ -0,0 +1,41 @@
+
+let product = {
+  id : "101",
+  name : "Mobile",
+  variation : [
+    {color : "black", prize : "$50"},
+    {color : "white", prize : "$60"},
+    {color : "golden", prize : "$70"},
+    {color : "green", prize : "$80"},
+  ],
+  reviews :[
+    {id :1 , name : "taimoor", comment :" good product", ratings : 4, status : true},
+    {id :1 , name : "hassan", comment :"easy to use", ratings : 4.5, status : true},
+    {id :1 , name : "abdul rehman", comment :"average", ratings : 2, status : false},
+  ]
+};
+
+console.log(product.) 
+
+// -------------------------------------------------------------------------------------------
+// let product = {
+//   id : "101",
+//   name : "Mobile",
+//   variation : [
+//     {color : "black", prize : "$50"},
+//     {color : "white", prize : "$60"},
+//     {color : "golden", prize : "$70"},
+//     {color : "green", prize : "$80"},
+//   ]
+// };
+
+// console.log(product.variation[2].color)
+
+// ----------------------------------------------------------------------------------------------
+// let product = {
+//   id : "101",
+//   name : "Mobile",
+//   color : ["green", "black","white","golden"]
+// };
+
+// console.log(product.color[3])
