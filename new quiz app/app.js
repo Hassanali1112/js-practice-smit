2c7c1e13d5447fc28e7708764af474fefb6da9ac
hassan ali <hassanali0111272@gmail.com>
2024-12-01 22:25:32 +0500

    basic layout

---
+++ new quiz app/app.js
@@ -0,0 +1,31 @@
+var htmlQuiz = [
+    {
+        question: 'HTML Stands for ?' ,
+        option1: 'Hypertext' ,
+        option2: 'Markup' , 
+        option3: 'HyperSuperText' , 
+        option4: 'HyperText Markup Language' , 
+        answer: 'HyperText Markup Language'
+    },
+    {
+        question: 'Which Element used to bold text in HTML?' ,
+        option1: 'img' ,
+        option2: 'h1' , 
+        option3: 'strong' , 
+        option4: 'p' , 
+        answer: 'strong'
+    },
+    {
+        question: 'Which Element used for Image in HTML?' ,
+        option1: 'span' ,
+        option2: 'div' , 
+        option3: 'image' , 
+        option4: 'img' , 
+        answer: 'img'
+    },
+
+]
+
+let quizContainer = document.querySelector(".quiz-container");
+
+fun
