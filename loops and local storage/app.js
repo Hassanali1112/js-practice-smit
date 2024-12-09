786b12f0afe0dd1751f83939ed8918581015c3d0
hassan ali <hassanali0111272@gmail.com>
2024-12-09 12:40:58 +0500

    obj getting setting done

--- loops and local storage/app.js
+++ loops and local storage/app.js
@@ -1,8 +1,37 @@
 // local storage
-localStorage.setItem("name", "hassan ali");
-let value = localStorage.getItem("name");
+// let set = document.querySelector("#set")
 
-console.log(value)
+let person = {
+    name :  "Hassan Ali",
+    age : 23,
+    course: "Web and app Development"
+}
+// set.addEventListener("click", ()=>{
+//     let setData = JSON.stringify(person)
+//     localStorage.setItem("data", setData)
+// })
+
+
+// let get = document.querySelector("#get")
+// get.addEventListener("click", ()=>{
+//     let response = localStorage.getItem("data")
+//     console.log(response)
+//     finalData = JSON.parse(response);
+//     console.log(finalData)
+// })
+ let data = JSON.stringify(person);
+ localStorage.setItem("data", data);
+
+ let response = localStorage.getItem("data")
+ let responseObj = JSON.parse(response)
+ console.log(responseObj)
+
+
+
+// localStorage.setItem("name", "hassan ali");
+// let value = localStorage.getItem("name");
+
+
 //  loops
 
 // for (var i = 0; i <= 3; i++) {
