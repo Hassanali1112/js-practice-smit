7cc9b2ead53a338b5e61af2a71a8791fd5d29fc2
hassan ali <hassanali0111272@gmail.com>
2024-12-09 16:15:08 +0500

    switching light dark mode

--- loops and local storage/app.js
+++ loops and local storage/app.js
@@ -1,11 +1,56 @@
+// light mode dark mode
+let dark = document.querySelector("#dark")
+
+dark.addEventListener("click",()=>{
+    let dark = localStorage.setItem("mode","dark")
+    setMode()
+
+})
+
+let light = document.querySelector("#light")
+light.addEventListener("click",()=>{
+    let light = localStorage.setItem("mode","light")
+    setMode()
+})
+
+let body = document.querySelector("body")
+
+
+let setMode = ()=>{
+    let currentMode = localStorage.getItem("mode")
+    // alert(currentMode)
+    // alert(currentMode)
+    if(currentMode === "dark"){
+        body.style.backgroundColor = "black"
+        // alert("light")
+    } else {
+        body.style.backgroundColor = "white"
+        // alert('dark')
+    }
+}
+
+let checkDefault = ()=>{
+    let currentMode = localStorage.getItem("mode")
+     if(currentMode === null){
+        body.style.backgroundColor = "white";
+        setMode()
+    } else{
+        setMode()
+    }
+
+}
+window.onload = checkDefault()
+
+
+// -----------------------------------------------------------------------------------
 // local storage
 // let set = document.querySelector("#set")
 
-let person = {
-    name :  "Hassan Ali",
-    age : 23,
-    course: "Web and app Development"
-}
+// let person = {
+//     name :  "Hassan Ali",
+//     age : 23,
+//     course: "Web and app Development"
+// }
 // set.addEventListener("click", ()=>{
 //     let setData = JSON.stringify(person)
 //     localStorage.setItem("data", setData)
@@ -19,12 +64,12 @@ let person = {
 //     finalData = JSON.parse(response);
 //     console.log(finalData)
 // })
- let data = JSON.stringify(person);
- localStorage.setItem("data", data);
+//  let data = JSON.stringify(person);
+//  localStorage.setItem("data", data);
 
- let response = localStorage.getItem("data")
- let responseObj = JSON.parse(response)
- console.log(responseObj)
+//  let response = localStorage.getItem("data")
+//  let responseObj = JSON.parse(response)
+//  console.log(responseObj)
 
 
 
