c0b4d075a230ac277ccc1739cd50a4fd54453f76
hassan ali <hassanali0111272@gmail.com>
2024-12-09 11:34:51 +0500

    getting and setting local storage

--- loops and local storage/app.js
+++ loops and local storage/app.js
@@ -1,3 +1,10 @@
+// local storage
+localStorage.setItem("name", "hassan ali");
+let value = localStorage.getItem("name");
+
+console.log(value)
+//  loops
+
 // for (var i = 0; i <= 3; i++) {
 //   console.log(i);
 // }
@@ -14,18 +21,18 @@
 //     i++
 // }while(i <= 3)
 
-for (var i = 4; i <= 3; i++) {
-  console.log(i);
-}
+// for (var i = 4; i <= 3; i++) {
+//   console.log(i);
+// }
 
-var i = 4
-while (i <= 3) {
-  console.log(i);
-  i++;
-}
+// var i = 4
+// while (i <= 3) {
+//   console.log(i);
+//   i++;
+// }
 
-var i = 4;
-do{
-    console.log(i)
-    i++
-}while(i <= 3)
+// var i = 4;
+// do{
+//     console.log(i)
+//     i++
+// }while(i <= 3)
