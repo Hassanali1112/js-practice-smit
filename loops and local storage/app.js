e750addfcae9c3685397ab33e4a5e8a5cb1e1b57
hassan ali <hassanali0111272@gmail.com>
2024-12-09 11:28:26 +0500

    only do while run once

--- loops and local storage/app.js
+++ loops and local storage/app.js
@@ -1,14 +1,30 @@
-for (var i = 0; i <= 3; i++) {
+// for (var i = 0; i <= 3; i++) {
+//   console.log(i);
+// }
+
+// var i = 0;
+// while (i <= 3) {
+//   console.log(i);
+//   i++;
+// }
+
+// var i = 0;
+// do{
+//     console.log(i)
+//     i++
+// }while(i <= 3)
+
+for (var i = 4; i <= 3; i++) {
   console.log(i);
 }
 
-var i = 0;
+var i = 4
 while (i <= 3) {
   console.log(i);
   i++;
 }
 
-var i = 0;
+var i = 4;
 do{
     console.log(i)
     i++
