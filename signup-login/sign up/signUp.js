4793152843ebb0bc86aa1f0c09d41ed9384e05f6
hassan ali <hassanali0111272@gmail.com>
2024-12-16 15:45:25 +0500

    signUp done

---
+++ signup-login/sign up/signUp.js
@@ -0,0 +1,35 @@
+let signUpName = document.querySelector("#signUpName");
+let signUpEmail = document.querySelector("#signUpEmail");
+let signUpNumber = document.querySelector("#signUpNumber");
+let signUpPassword = document.querySelector("#signUpPassword");
+let signUpRePassword = document.querySelector("#signUpRePassword");
+
+let signUpNameRegex = /^[a-zA-Z]+$/;
+let signUpEmailRegex = /^\S+@\S+\.\S+$/;
+let signUpNumberRegex = /^((\+92)?(0092)?(92)?(0)?)(3)([0-9]{9})$/;
+
+let signUpBtn = document.querySelector(".signUpBtn");
+
+signUpBtn.addEventListener("click", () => {
+  if (signUpNameRegex.test(signUpName.value)) {
+    if (signUpEmailRegex.test(signUpEmail.value)) {
+      if (signUpNumberRegex.test(signUpNumber.value)) {
+        if (!signUpPassword.value == "") {
+          if (signUpRePassword.value == signUpPassword.value) {
+            alert("user Registered successfully");
+          } else {
+            alert("password does not match");
+          }
+        } else {
+          alert("Password should not be null");
+        }
+      } else {
+        alert("Please enter a valid Pakistani Phone Number");
+      }
+    } else {
+      alert("Please enter a valid Email address");
+    }
+  } else {
+    alert("Please enter a valid Name");
+  }
+});
